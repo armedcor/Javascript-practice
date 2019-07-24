488c8069fe1dda31fe5af70354409e6acde85f35
Cormac Lawlor <armedcor@gmail.com>
2019-07-24 22:50:46 +0100

    Basic Operators

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ console.log(job);
 */
 
 // Variable mutation and type coercion
-
+/*
 var firstName = 'John'
 var age = 28;
 
@@ -40,4 +40,36 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+*/
 
+
+/*********************
+ * Basic Operators
+ ********************/
+var year, yearJohn, yearMark;
+now = 2019;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+
+ console.log(yearJohn);
+ console.log(now + 2);
+ console.log(now * 2);
+ console.log(now / 10);
+
+ // Logical operators
+
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
