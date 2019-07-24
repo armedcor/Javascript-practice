f4d16d67b7396498595ca59981936575e9cf5cb5
Cormac Lawlor <armedcor@gmail.com>
2019-07-24 22:23:31 +0100

    Variable Mutation and Type coercion

--- script.js
+++ script.js
@@ -37,3 +37,7 @@ age = 'twenty eight';
 job = 'driver';
 
 alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+
