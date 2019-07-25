02cd295183848855ab5ea8d4eb45a172aa8a2756
Cormac Lawlor <armedcor@gmail.com>
2019-07-26 00:00:27 +0100

    Boolean logic

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ console.log(x);
  /***
   * If/Else statement
   */
-
+/*
   var firstName = 'John';
   var civilStatus = 'single';
 
@@ -161,3 +161,22 @@ if (markBmi > johnBmi) {
 } else {
     console.log("John\'s BMI is higher than Mark\'s.");
 }
+
+*/
+
+/**
+ * Boolean logic
+ */
+
+ var firstName = 'John';
+ var age = 20;
+
+ if (age < 13) {
+     console.log(firstName + ' is a boy');
+ } else if (age >= 13 && age < 20) { // Between 13 and 20
+     console.log(firstName + ' is a teenager.')
+ } else if (age > 20 && age < 30) {
+     console.log(firstName + ' is a young man.')
+ }else {
+     console.log(firstName + ' is a man');
+ }
